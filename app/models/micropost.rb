8f0bb60ccb47f635c6cc33e21f1e831eaa5784ed
ssszz_linux <spachitralada@gmail.com>
2023-01-07 10:32:01 +0700

    remove user validate that make my web bug

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ApplicationRecord
     belongs_to :user
-    validates :content, length: { maximum: 140 }, ## ยาวสุด 140 char
-                        presence: true
-end
+    validates :content, length: { maximum: 140 } , presence: true 
+end## ยาวสุด 140 char
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates FILL_IN, presence: true ## replace fill in with right code
-    validates FILL_IN, presence: true 
+    #validates FILL_IN, presence: true # replace fill in with the right code
+    #validates FILL_IN, presence: true # replace fill in with the right code
 end
